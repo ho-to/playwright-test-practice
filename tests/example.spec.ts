d387a54ca7cbc390383d57749502c6b148fc23ec
ho-to <149668507+ho-to@users.noreply.github.com>
2026-09-03 12:56:58 +0900

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,9 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-test('example.comが表示される', async ({ page }) => {
-  await page.goto('https://example.com');
-
-  await expect(page).toHaveTitle('Example Domain');
-
-  await expect(page.locator('h1')).toHaveText('Example Domain');
+test('test', async ({ page }) => {
+  await page.goto('https://example.com/');
+  await page.getByRole('link', { name: 'Learn more' }).click();
+  await page.getByRole('link', { name: 'IANA-managed Reserved Domains' }).click();
+  await page.getByText('Certain domains are set aside').click();
+  await page.getByRole('heading', { name: 'IANA-managed Reserved Domains' }).click();
+  await page.getByRole('heading', { name: 'IANA-managed Reserved Domains' }).click();
+  await page.getByRole('heading', { name: 'IANA-managed Reserved Domains' }).click();
+  await page.getByRole('heading', { name: 'Example domains' }).click();
+  await page.getByRole('link', { name: 'Special-Use Domain Names' }).nth(1).click();
 });
